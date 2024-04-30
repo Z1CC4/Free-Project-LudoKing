1bf32bd1d6cc4651efb088c8e5ba776c86e87e63
Francesco Masala <fmasala@hyperbit.it>
2024-04-30 16:50:40 +0200

    Demo commit and pull request #1

---
+++ src/main/java/it/unibz/wie2024/demo1/HelloController.java
@@ -0,0 +1,18 @@
+package it.unibz.wie2024.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void onGoodbyeButtonClick() {
+        welcomeText.setText("Goodbye!");
+    }
+}
