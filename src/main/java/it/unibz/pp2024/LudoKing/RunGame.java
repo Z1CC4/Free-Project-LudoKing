ee56c59929d03208038f0e0fa38d64dba7946da3
Francesco Masala <fmasala@hyperbit.it>
2024-06-24 19:52:47 +0200

    Added main class and cleaned some folders

---
+++ src/main/java/it/unibz/pp2024/LudoKing/RunGame.java
@@ -0,0 +1,11 @@
+package it.unibz.pp2024.LudoKing;
+
+
+// Class created to run the game
+// TODO: Assign this class to be the main class of the runnable jar
+public class RunGame {
+    public static void main(String[] args) {
+        System.Logger logger = System.getLogger("RunGame");
+        logger.log(System.Logger.Level.INFO, "Starting LudoKing game");
+    }
+}
