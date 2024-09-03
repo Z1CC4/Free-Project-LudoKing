7340e201478128cc8cb5128971d1d3749b355256
Matteo Hu <matthu@unibz.it>
2024-09-03 18:06:22 +0200

    additional changes

--- src/main/java/it/unibz/pp2024/LudoKing/gameLogic/games/miniGames/NewGTW.java
+++ src/main/java/it/unibz/pp2024/LudoKing/gameLogic/games/miniGames/NewGTW.java
@@ -7,6 +7,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NewGTW extends MiniGame {
+
+    private static Random rand = new Random();
+    Scanner sc = new Scanner(System.in);
+    private static final int attemptsMax=20;
+    private static final int secretWordLength=4;
+    private static char[] characters = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+    private static String secretWord=generateSecretWord();
+
     public static void returnPoints(Player player) {
         int pointsToAdd = 100;
         System.out.println("You obtained: " + pointsToAdd + " points.");
@@ -15,9 +24,18 @@ public class NewGTW extends MiniGame {
 
     @Override
     public boolean play(Player p) {
-        Random rand = new Random();
-        Scanner sc = new Scanner(System.in);
-
         return false;
+    }
+
+    public static void game(){
+
+    }
+
+    public static String generateSecretWord(){
+        String name="";
+        for(int i=0;i<=secretWordLength;i++){
+            name+=characters[rand.nextInt(0,characters.length)];
+        }
+        return name;
     }
 }
